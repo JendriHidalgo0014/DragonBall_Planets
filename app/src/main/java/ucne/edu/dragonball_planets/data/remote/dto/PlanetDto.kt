dff79ef485716cd15e3ea4fe81e02ffbf1712299
Jendri Jose Hidalgo Rosario <jendri_hidalgo@ucne.edu.do>
2026-02-21 23:39:32 -0400

    Se creó el Dto de planeta.

---
+++ app/src/main/java/ucne/edu/dragonball_planets/data/remote/dto/PlanetDto.kt
@@ -0,0 +1,21 @@
+package ucne.edu.dragonball_planets.data.remote.dto
+
+import ucne.edu.dragonball_planets.domain.model.Planet
+data class PlanetResponseDto(
+    val items: List<PlanetDto>
+)
+data class PlanetDto(
+    val id: Int,
+    val name: String,
+    val isDestroyed: Boolean,
+    val description: String,
+    val image: String
+) {
+    fun toDomain() = Planet(
+        id = id,
+        name = name,
+        isDestroyed = isDestroyed,
+        description = description,
+        image = image
+    )
+}
