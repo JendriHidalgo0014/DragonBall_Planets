4b199900fbe0a514b0ef55717ad6d3bead455bdf
Jendri Jose Hidalgo Rosario <jendri_hidalgo@ucne.edu.do>
2026-02-21 23:41:15 -0400

    Se creó GetPlanetsUseCase.

---
+++ app/src/main/java/ucne/edu/dragonball_planets/domain/usecase/GetPlanetsUseCase.kt
@@ -0,0 +1,14 @@
+package ucne.edu.dragonball_planets.domain.usecase
+
+import ucne.edu.dragonball_planets.domain.repository.PlanetRepository
+import javax.inject.Inject
+
+class GetPlanetsUseCase @Inject constructor(
+    private val repository: PlanetRepository
+) {
+    suspend operator fun invoke(
+        page: Int = 1,
+        limit: Int = 10,
+        name: String? = null
+    ) = repository.getPlanets(page, limit, name)
+}
